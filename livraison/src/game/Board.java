afd519fb23324e46b7ca612a7b7a598de9e1328f
Mori Baptiste <21602052@C304L-138C12.campus.unicaen.fr>
2018-10-09 15:19:18 +0200

    board

--- livraison/src/game/Board.java
+++ livraison/src/game/Board.java
@@ -1,4 +1,13 @@
 package game;
 
 public class Board {
+  private Tile[][] grid;
+
+  public Board(int x, int y){
+    this.grid = generateGrid(x,y);
+  }
+
+  public Board(){
+    this.grid = generateGrid(0,0);
+  }
 }
