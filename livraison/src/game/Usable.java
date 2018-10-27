17cfa526695efedbb017a6a25fd5c47bbda72284
Elsion14 <dchagneux@live.fr>
2018-10-27 16:49:19 +0200

    Usable

---
+++ livraison/src/game/Usable.java
@@ -0,0 +1,15 @@
+package game;
+
+public abstract class Usable extends AbstractTile {
+
+  protected boolean isActivable;
+
+  public Usable(Position pos, boolean isActivable) {
+    super(pos,false);
+    this.isActivable = isActivable;
+  }
+
+  public abstract void action();
+
+  public abstract boolean isActivable();
+}
