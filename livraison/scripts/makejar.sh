c80fc989eb64b76ec389ad201e9b83273db64edd
ValentinLe <valou.leblond1@gmail.com>
2018-11-25 23:30:32 +0100

    scripts doc et jar

---
+++ livraison/scripts/makejar.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd $(dirname $0)/..
+sh scripts/compile.sh
+[ -d jar ] || mkdir jar
+[ -d jar/dist ] || mkdir jar/dist
+
+nameArchive="Archive.jar"
+jar cf $nameArchive build/ src/
+mv $nameArchive jar/dist/
