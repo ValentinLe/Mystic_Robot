39a8a2322579ba5dc6ff2c9e2eb5ab22646415fa
nicolaks <nico145702@gmail.com>
2018-11-25 22:29:37 +0100

    Javadoc gun

--- livraison/src/game/Gun.java
+++ livraison/src/game/Gun.java
@@ -2,15 +2,34 @@ package game;
 
 public class Gun extends Weapon {
 
+  /**
+    * Constructeur de la classe
+    * @param type , qui est de type String.
+    * @param damage , qui est de type int.
+    * @param owner , qui est de type Player.
+    *
+    */
   public Gun(String type, int damage, int range, Player owner) {
     super(type, damage, range, owner);
   }
 
+  /**
+    * Méthode permettant de retourner les dommages.
+    * @return this.damage
+    *
+    *
+    */
   @Override
   public int getDamage() {
     return this.damage;
   }
 
+  /**
+    * Méthode permettant de retourner la portée de l'arme.
+    * @return this.range 
+    *
+    *
+    */
   @Override
   public int getRange() {
     return this.range;
