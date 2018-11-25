8459a14f34142cd681ddb9761b38be303533c8e6
nicolaks <nico145702@gmail.com>
2018-11-25 22:24:41 +0100

    Javadoc Equipement, explosif

--- livraison/src/game/Equipement.java
+++ livraison/src/game/Equipement.java
@@ -7,11 +7,35 @@ import space.Direction;
 public interface Equipement {
     public boolean use(Position position,Direction direction,Game board);
 
+    /**
+      * Méthode permettant de retourner le Type.
+      *
+      *
+      *
+      */
     public String getType();
 
+    /**
+      * Méthode permettant de retourner "Owner"
+      *
+      *
+      *
+      */
     public Player getOwner();
 
+    /**
+      * Méthode permettant de mettre en place un proprietaire.
+      * un "Owner"
+      *
+      *
+      */
     public void setOwner(Player player);
 
+    /**
+      * Methode renvoyant une copie.
+      *
+      *
+      *
+      */
     public Equipement getCopy();
 }
--- livraison/src/game/Explosif.java
+++ livraison/src/game/Explosif.java
@@ -6,15 +6,35 @@ import java.util.*;
 
 public abstract class Explosif extends Weapon {
 
+  /**
+    * Constructeur de la classe
+    * @param type , de type String.
+    * @param damage , qui est de type int.
+    * @param range , qui est de type int.
+    * @param owner , qui est de type Player.
+    */
   public Explosif(String type, int damage, int range, Player owner) {
     super(type, damage, range, owner);
   }
 
+  /**
+    * Méthode renvoyant les dommages.
+    *@return this.damage
+    *
+    *
+    */
   @Override
   public int getDamage() {
     return this.damage;
   }
 
+  /**
+    * Méthode permettant de utiliser un equipement.
+    * @param posiion , qui est de type Position.
+    * @param direction , qui est de type Direction.
+    * @param board , qui est de type Game.
+    * @return true .
+    */
   @Override
   public boolean use(Position position,Direction direction,Game board){
     Position newPosition = new Position(position.getX()+direction.getX(),position.getY()+direction.getY());
@@ -22,6 +42,12 @@ public abstract class Explosif extends Weapon {
     return true;
   }
 
+  /**
+    * Méthode permettant d'appliquer les dommages.
+    * @param board , qui est de type RealGame.
+    * @param explosifPosition , qui est de type Position.
+    * @param direction , qui est de type Position.
+    */
   public void applyDamage(RealGame board, Position explosifPosition, Position direction) {
     List<Player> players = board.getPlayersAround(explosifPosition, this.range);
     for (Player player : players) {
@@ -30,6 +56,12 @@ public abstract class Explosif extends Weapon {
     }
   }
 
+  /**
+    * Méthode permettant de mettre en Tile
+    *
+    *
+    *
+    */
   public abstract Tile toTile(Position position);
 
 }
