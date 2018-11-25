b3a8961218ff4b81c758f9ba98662761b3a045c9
Elsion14 <dchagneux@live.fr>
2018-11-25 22:43:30 +0100

    Javadoc Tile et Usable

--- livraison/src/game/Tile.java
+++ livraison/src/game/Tile.java
@@ -4,11 +4,27 @@ import space.Position;
 
 public interface Tile {
 
+  /**
+    * Retourne la position de l'objet dans la grille
+    *@return la position de l'objet
+    */
   public Position getPosition();
 
+  /**
+    * Retourne si l'objet est un obstacle
+    *@return si l'objet est un obstacle
+    */
   public boolean getIsObstacle();
 
+  /**
+    * Modifie l'attribut isObstacle
+    *@param isObstacle un booléen
+    */
   public void setIsObstacle(boolean isObstacle);
 
+  /**
+    * Retourne la représentation de l'objet sous forme de String
+    *@return la représentation de l'objet
+    */
   public String getType();
 }
--- livraison/src/game/Usable.java
+++ livraison/src/game/Usable.java
@@ -8,18 +8,37 @@ public abstract class Usable extends AbstractTile {
   protected boolean isActivable;
   protected int range;
 
+  /**
+    * Le constructeur de la classe
+    *@param type le type de l'objet
+    *@param pos la position dans la grille
+    *@param isActivable si l'objet est activable ou non
+    *@param range la portée de l'objet
+    */
   public Usable(String type, Position pos, boolean isActivable, int range) {
     super(type,pos, false);
     this.isActivable = isActivable;
     this.range = range;
   }
 
+  /**
+    * Retourne la valeur de l'attribut range
+    *@return la valeur de l'attribut range
+    */
   public int getRange() {
     return this.range;
   }
 
+  /**
+    * Méthode abstraite permettant d'effectuer une action
+    *@param board une instance de RealGame
+    */
   public abstract void action(RealGame board);
 
+  /**
+    * Retourne si l'objet est activable
+    *@return si l'objet est activable
+    */
   public boolean isActivable() {
     return this.isActivable;
   }
