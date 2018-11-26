adcbdd1d15e5cc48d67ba4c0ac81942967c196ec
Mori Baptiste <moribaptiste872@gmail.com>
2018-11-26 20:00:00 +0100

    javadoc ia et proxygame

--- livraison/src/game/IA.java
+++ livraison/src/game/IA.java
@@ -2,6 +2,11 @@ package game;
 
 public interface IA {
 
+  /**
+   * Fais jouer un robot avec comme priorité d'attaquer un autre robot si celui
+   * ci est à ça portée sinon il choisit une action aléatoirement
+   * @param player la position de départ
+   */
   public void execute(Player player);
 
 }
--- livraison/src/game/ProxyGame.java
+++ livraison/src/game/ProxyGame.java
@@ -10,6 +10,11 @@ public class ProxyGame implements Game {
   private Game game;
   private Player player;
 
+  /**
+   * creer une instance de proxyboard
+   * @param game la vision du proxy 
+   * @param player le joueur qui voit le proxy
+   */
   public ProxyGame(Game game, Player player) {
     this.game = game;
     this.player = player;
@@ -26,6 +31,11 @@ public class ProxyGame implements Game {
     this.game.addModelListener(l);
   }
 
+  /**
+   * Getter de la case présente à la position
+   * @param position la position dont on veut la case
+   * @return la case ciblée
+   */
   @Override
   public Tile getTileAt(Position position) {
     Tile tile = this.game.getTileAt(position);
@@ -41,76 +51,145 @@ public class ProxyGame implements Game {
     }
   }
 
+  /**
+   * Test si la coordonnée (x,y) est dans la grille
+   * @param x la coordonnée en absisce
+   * @param y la coordonnée en ordonnée
+   * @return true si (x,y) est dans la grille
+   */
   @Override
   public boolean isInIndex(Position position){
     return this.game.isInIndex(position);
   }
 
+  /**
+   * test si un joueur peut se déplacer sur la nouvelle position
+   * @param position la position du test
+   * @return true si le peux se déplacer à la nouvelle position
+   */
   @Override
   public boolean canMove(Position position) {
     return this.game.canMove(position);
   }
 
+  /**
+   * Recupére les cases depuis une position dans une direction selon une portée
+   * @param position la position de départ
+   * @param direction la direction dans laquelle aller chercher les cases
+   * @param range la porté (le nombre de cases à avoir)
+   * @return la liste des cases voulues
+   */
   @Override
   public List<Tile> getTileInDirection(Position position, Direction direction, int range){
     return this.game.getTileInDirection(position,direction,range);
   }
 
+  /**
+   * retourne le player qui se trouve dans la direction indiqué
+   * @param position position à partir de laquelle on cherche
+   * @param direction direction dans laquelle chercher
+   * @param range jusqu'où cherché dans la direction voulus
+   * @return le player trouvé
+   */
   @Override
   public Player getPlayerInDirection(Position position, Direction direction, int range){
     return this.game.getPlayerInDirection(position,direction,range);
   }
 
+  /**
+   * Passe le tour du joueur courant
+   */
   @Override
   public void skipTurn(){
     this.game.skipTurn();
   }
 
+  /**
+   * remplace une Tile par la tile passé en paramêtre
+   * @param tile la Tile par laquelle on veut remplacer
+   */
   @Override
   public void setTile(Tile tile) {
     this.game.setTile(tile);
   }
 
+  /**
+   * Active la case avec la liste des joueurs dans le voisinage de la case si
+   * c'est une instance d'usable
+   * @param tile la case à éventuellement activer
+   */
   @Override
   public void activate(Tile tile) {
     this.game.activate(tile);
   }
 
+  /**
+   * place le joueur en tete de file à la fin de celle-ci
+   */
   @Override
   public void switchPlayer() {
     this.game.switchPlayer();
   }
 
+  /**
+   * Retourne la map de jeu
+   * @return Tile[][]
+   */
   @Override
   public Tile[][] getGrid() {
     return this.game.getGrid();
   }
 
+  /**
+  * récupère le premier joueur de la file
+  * @return le prochain joueur qui doit jouer
+  */
   @Override
   public Player getNextPlayer() {
     return this.game.getNextPlayer();
   }
 
+  /**
+   * Retourne la largeur de la map
+   * @return int
+   */
   @Override
   public int getWidth() {
     return this.game.getWidth();
   }
 
+  /**
+   * Retourne la hauteur de la map
+   * @return int
+   */
   @Override
   public int getHeight() {
     return this.game.getHeight();
   }
 
+  /**
+   * Retourne la liste des joueurs sur la map
+   * @return la liste des joueurs
+   */
   @Override
   public List<Player> getListPlayers() {
     return this.game.getListPlayers();
   }
 
+  /**
+   * Retourne l'équipement du joueur courrant
+   * @return le map d'équipement du joueur
+   */
   @Override
   public Map<Equipement, Integer> getEquipementOfPlayer() {
     return this.player.getEquipement();
   }
 
+  /**
+   * fonction toString permet de retourner la grille de jeu sous forme de chaine
+   * de caractères
+   * @return String
+   */
   @Override
   public String toString() {
     String res = "  ";
